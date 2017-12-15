978f338368abe2fd543ed5716abba48338376d4b
CricketXV <33155889+CricketXV@users.noreply.github.com>
2017-12-15 09:56:24 -0500

    finished

--- BoxesCyka/Program.cs
+++ BoxesCyka/Program.cs
@@ -6,6 +6,7 @@ namespace BoxesCyka
 	{
 		public static void Main (string[] args)
 		{
+			START:
 			Console.Write ("State length:");
 			int length = Convert.ToInt32 (Console.ReadLine ());
 			Console.Write ("State width:");
@@ -15,6 +16,17 @@ namespace BoxesCyka
 			Console.WriteLine ("Your length is: " + length);
 			Console.WriteLine ("Your width is: " + width);
 			Console.WriteLine ("Your symbol: " + symbol);
+
+			for (int j = 0; j < length; j++) 
+			{
+				for ( int i = 0; i < width; i++ )
+				{
+					Console.Write (symbol);
+				}
+				Console.WriteLine (" ");
+			}
+			Console.WriteLine ("\n");
+			goto START;
 		}
 	}
 }
