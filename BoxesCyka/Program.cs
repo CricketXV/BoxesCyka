d2e792be83d7ba23428b57229dfa641f9713775b
CricketXV <33155889+CricketXV@users.noreply.github.com>
2017-12-15 09:28:48 -0500

    Initial commit

---
+++ BoxesCyka/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BoxesCyka
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.Write ("State length:");
+			int length = Convert.ToInt32 (Console.ReadLine ());
+			Console.Write ("State width:");
+			int width = Convert.ToInt32 (Console.ReadLine ());
+			Console.Write ("Symbol: ");
+			char symbol = Convert.ToChar (Console.ReadLine ());
+			Console.WriteLine ("Your length is: " + length);
+			Console.WriteLine ("Your width is: " + width);
+			Console.WriteLine ("Your symbol: " + symbol);
+		}
+	}
+}
